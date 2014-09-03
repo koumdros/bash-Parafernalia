7f8b6b47d0111cd8169f5661983cfdd397b3b56a
koumdros <koumdros@gmail.com>
2014-09-03 23:54:12 +0300

    checks the status of the offloading settings of NICs in XenServer

--- scripts/XenServer/Check_VM_Net.sh
+++ scripts/XenServer/Check_VM_Net.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#checks the status of the offloading settings of NICs in XenServer
 for INSTANCEID in`xe vm-list | awk '/name-label/' | grep -v "Control domain" | awk '{print $4}'`
   do
     VMUUID=`xe vm-list name-label=${INSTANCEID} | awk '/uuid/ {print $5}'`
