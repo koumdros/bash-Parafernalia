597b32ef3b63f05264639ed4f28c08f04794c58e
koumdros <koumdros@gmail.com>
2014-09-03 23:52:51 +0300

    Create Check_VM_Net.sh

---
+++ scripts/XenServer/Check_VM_Net.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for INSTANCEID in`xe vm-list | awk '/name-label/' | grep -v "Control domain" | awk '{print $4}'`
+  do
+    VMUUID=`xe vm-list name-label=${INSTANCEID} | awk '/uuid/ {print $5}'`
+      for VIFUUID in`xe vif-list vm-uuid=$VMUUID | awk '/uuid/ {print $5}' | sed '/^$/d'`
+        do
+  	  xe pif-param-list uuid=$VIFUUID | grep "other-config"
+     	done
+  done
