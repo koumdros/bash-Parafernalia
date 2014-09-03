45f6a4388193d05dd4e1636625a855b2b7afbb53
koumdros <koumdros@gmail.com>
2014-09-03 23:58:14 +0300

    fix TCP offload incompatibility for Xenserver 6.1
    by turning it off for all pifs

---
+++ scripts/XenServer/FixNet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script attempts to fix the TCP offload incomaptibility for Xenserver 6.1
+## by turning it off for all pifs
+for PIFUUID in`xe pif-list | awk '/uuid/ {print $5}' | sed '/^$/d'`
+  do
+    xe pif-param-set uuid=$PIFUUID other-config:ethtool-gso="off";
+    xe pif-param-set uuid=$PIFUUID other-config:ethtool-ufo="off";
+    xe pif-param-set uuid=$PIFUUID other-config:ethtool-tso="off";
+    xe pif-param-set uuid=$PIFUUID other-config:ethtool-sg="off";
+    xe pif-param-set uuid=$PIFUUID other-config:ethtool-tx="off";
+    xe pif-param-set uuid=$PIFUUID other-config:ethtool-rx="off";
+done
